c5d006fb530a8c89c66a7479ba56380835e89ac9
javier fernandez <jf2876430@gmail.com>
2024-11-17 13:26:24 -0400

    responsive listo y  animaciones

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "2px -3px 19px 0 rgba(255, 255, 255, 0)",
+      },
+    },
   },
   plugins: [],
 };
