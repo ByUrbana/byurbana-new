11e1a84b4b052bb1fb1f996eddf428404732f6ed
javier fernandez <jf2876430@gmail.com>
2024-12-07 02:39:33 -0400

    cambios mobile detalles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      boxShadow: {
-        "3xl": "2px -3px 19px 0 rgba(255, 255, 255, 0)",
-      },
+    fontFamily: {
+      CoFoNormal: ["CoFo Sans Normal", "sans-serif"],
+      CoFoBold: ["CoFo-Sans-Bold", "sans-serif"],
+      CoFoMedium: ["CoFo Sans Medium", "sans-serif"],
+      CoFoBlack: ["CoFo Sans Black", "sans-serif"],
     },
   },
   plugins: [],
